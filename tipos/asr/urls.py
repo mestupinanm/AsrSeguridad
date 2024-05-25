eb40991b10090785e217f3a7107addc8bf058748
“Leonardo”3310 <jl.rangel@uniandes.edu.co>
2024-05-25 10:20:24 -0500

    x

--- tipos/asr/urls.py
+++ tipos/asr/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tipos.urls')),
+    path('', include('documentos.urls')),
 ]
 
 if settings.DEBUG is True:
