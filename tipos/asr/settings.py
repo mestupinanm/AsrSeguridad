7e604e7aa4222ac979663f22d78d1e36fc74290d
“Leonardo”3310 <jl.rangel@uniandes.edu.co>
2024-05-25 23:29:37 -0500

    em

--- tipos/asr/settings.py
+++ tipos/asr/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'asr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'asr/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
