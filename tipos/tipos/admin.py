41259f1e247cc49b36501a5e640bd7baa3001614
“Leonardo”3310 <jl.rangel@uniandes.edu.co>
2024-05-25 23:05:47 -0500

    x

--- tipos/tipos/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . models import Tipo
-
-# Register your models here.
-admin.site.register(Tipo)
--- tipos/tipos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TiposConfig(AppConfig):
-    name = 'tipos'
